3fbee6a731579fdad9e43395de46b80fa83d2d5c
AmbitiondX <40709864+AmbitiondX@users.noreply.github.com>
2021-11-30 21:28:57 +0800

    Delete gmall-realtime-exer3 directory

--- gmall-realtime-exer3/src/main/java/com/atguigu/app/func/JoinDimFunction.java
+++
@@ -1,8 +0,0 @@
-package com.atguigu.app.func;
-
-import com.alibaba.fastjson.JSONObject;
-
-public interface JoinDimFunction<T> {
-    String getKey(T input);
-    void join(T input, JSONObject dimInfo);
-}
